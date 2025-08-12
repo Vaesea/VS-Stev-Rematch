6ac81ac3d4696c53ab254aab3c94bbe05e4a510c
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-08-12 14:55:17 +0100

    Update CreditsState.hx
    [ci skip]

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -38,6 +38,10 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			["VS Stev Rematch Credits"],
+			["Vaesea",				"vaesea",			"Main Creator of VS Stev Rematch",						"https://www.youtube.com/@VaeseaGJ",	"FFFFFF"],
+			["Your Average FNF Pro", "stev",			"Creator of VS Stev", 		"https://fridaynightfunking.fandom.com/wiki/Stev_(ur_avg_fnf_pro)",	"FFFFFF"],
+			[""],
 			["Psych Engine Team"],
 			["Shadow Mario",		"shadowmario",		"Main Programmer and Head of Psych Engine",					"https://ko-fi.com/shadowmario",	"444444"],
 			["Riveren",				"riveren",			"Main Artist/Animator of Psych Engine",						"https://x.com/riverennn",			"14967B"],
